ec778801809e9b48a2af7147723761460f89e489
Simmonsj10 <33575702+Simmonsj10@users.noreply.github.com>
2021-03-13 12:37:37 -0800

    Add files via upload

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -1,8 +1,9 @@
 library(dplyr)
 library(tidyverse)
 
+#Deliverable 1
 MechaCar_df <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-head(MechaCar_df)
+# head(MechaCar_df)
 #summary(lm(mpg ~ vehicle_length, MechaCar_df)) #linear model on vehicle_length
 #summary(lm(mpg ~ vehicle_weight, MechaCar_df)) #linear model on all vehicle_weight
 #summary(lm(mpg ~ spoiler_angle, MechaCar_df)) #linear model on spoiler_angle
@@ -11,16 +12,29 @@ head(MechaCar_df)
 summary(lm(mpg ~ ., data = MechaCar_df)) #linear model on all factors
 
 
-#cor(MechaCar_df)
-#plt <- ggplot(MechaCar_df,aes(x=mpg,y=vehicle_length))
-#plt + geom_point() #create a scatter plot
-#cor(MechaCar_df$mpg,MechaCar_df$vehicle_length) #calculate correlation coefficient
-
-
+#Deliverable 2
 SuspensionCoil_df <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-head(SuspensionCoil_df)
+# head(SuspensionCoil_df)
 total_summary <-summarize(SuspensionCoil_df, Mean_PSI = mean(SuspensionCoil_df$PSI), Median_PSI = median(SuspensionCoil_df$PSI), Variance_PSI = var(SuspensionCoil_df$PSI), Std_PSI = sd(SuspensionCoil_df$PSI))
-total_summary
-
 lot_summary <- SuspensionCoil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI = var(PSI), Std_PSI = sd(PSI))
-lot_summary
+
+total_summary #Print Total Summary
+lot_summary #Print Lot Summary
+
+
+#Deliverable 3
+L1 = SuspensionCoil_df %>% filter(Manufacturing_Lot=="Lot1") #Create Lot 1
+L2 = SuspensionCoil_df %>% filter(Manufacturing_Lot=="Lot2") #Create Lot 2
+L3 = SuspensionCoil_df %>% filter(Manufacturing_Lot=="Lot3") #Create Lot 3
+Total_population = SuspensionCoil_df$PSI
+
+Lot1_Test = t.test(Total_population, L1$PSI) #Run t.test of population mean v Lot 1
+Lot2_Test = t.test(Total_population, L2$PSI) #Run t.test of population mean v Lot 2
+Lot3_Test = t.test(Total_population, L3$PSI) #Run t.test of population mean v Lot 3
+
+Lot1_Test #Print T.Test on Lot 1
+Lot2_Test #Print T.Test on Lot 2
+Lot3_Test #Print T.Test on Lot 3
+
+
+
